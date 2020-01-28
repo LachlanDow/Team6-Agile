2dd9e7d57ae9eb914ab7077e23b29a1d44f3e404
ajholligan <33227000+ajholligan@users.noreply.github.com>
2020-01-28 14:18:30 +0000

    Added All current SQL Queries
    Including:
    Searching by a state
    Searching by a Procedure name
    Search by a procedure name then location
    Swarching by procedure name and within a price range

---
+++ SQL/searchNameByLocation.sql
@@ -0,0 +1,24 @@
+CREATE DEFINER=`cammymcn_AglAdm`@`%` PROCEDURE `searchNameByLocation`(in search varchar(50), IN state varchar(2))
+BEGIN
+SELECT DISTINCT
+    c.DRG,
+    c.Average_Total_Payments,
+    c.Total_Discharges,
+    v.Name,
+    v.Street_Address,
+    v.Zipcode,
+    v.HRR
+FROM
+    Procedures AS c,
+    Provider AS v
+WHERE
+    v.ProviderID = c.ProviderID
+        AND c.DRG IN (SELECT 
+            DRG
+        FROM
+            Procedures
+        WHERE
+            DRG LIKE CONCAT('%', search, '%'))
+		AND v.State IN (SELECT State FROM Provider WHERE State = state)
+ORDER BY c.DRG, c.Average_Total_Payments , c.Total_Discharges DESC;
+END
